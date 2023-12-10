cb81bfd287d2532ab961a489244e4f0445ec783d
edenede2 <131803027+edenede2@users.noreply.github.com>
2023-12-10 18:18:13 +0200

    Update rDoc_streamlit.py

--- rDoc_streamlit.py
+++ rDoc_streamlit.py
@@ -6,9 +6,11 @@ import openpyxl
 # Function to load and process the Excel file
 @st.cache_resource  # Updated caching function
 def load_excel(file):
-    # Reading the Excel file with the second row as header
-    df = pd.read_excel(file, header=1)
-    # Ensure that only numeric data is used in calculations
+    # Reading the Excel file and using the first row for multi-index columns
+    df = pd.read_excel(file, header=[0, 1])
+    # Flatten the MultiIndex for easier processing
+    df.columns = ['_'.join(col).strip() for col in df.columns.values]
+    # Convert all relevant columns to numeric, ignoring errors to skip non-numeric columns
     for col in df.columns[1:]:  # Assuming first column is not numeric
         df[col] = pd.to_numeric(df[col], errors='coerce')
     return df
@@ -24,37 +26,41 @@ def main():
         st.write("DataFrame Preview:")
         st.write(df)
 
-        # Metric selection (excluding 'Unnamed' options and the first column)
-        metric_options = df.columns[1:].tolist()  # Assuming first column is for subjects
-        selected_metric = st.selectbox("Select a Metric", metric_options)
+        # Extract metric names (assuming they are prefixes in the column names)
+        metric_names = list(set(col.split('_')[0] for col in df.columns if '_' in col))
+        selected_metric = st.selectbox("Select a Metric", metric_names)
+
+        # Extract segment names for the selected metric
+        segments = [col for col in df.columns if col.startswith(selected_metric)]
 
         # Plot type selection
         plot_types = ['line', 'bar', 'scatter']
         plot_type = st.selectbox("Select Plot Type", plot_types)
 
         # Subject exclusion or isolation
-        subjects = df.iloc[:, 0].unique()
+        subjects = df['Subject'].unique()
         exclude_subjects = st.multiselect("Exclude Subjects", subjects)
         isolate_subject = st.selectbox("Or, Isolate a Single Subject (overrides exclusion)", ['None'] + list(subjects))
 
         # Data processing for visualization
         if isolate_subject != 'None':
-            df_filtered = df[df.iloc[:, 0] == isolate_subject]
+            df_filtered = df[df['Subject'] == isolate_subject]
         else:
-            df_filtered = df if not exclude_subjects else df[~df.iloc[:, 0].isin(exclude_subjects)]
+            df_filtered = df if not exclude_subjects else df[~df['Subject'].isin(exclude_subjects)]
 
-        # Calculating mean across all subjects for each segment
-        segment_means = df_filtered.mean(numeric_only=True)
+        # Calculating mean across all subjects for each segment under the selected metric
+        segment_means = df_filtered[segments].mean()
 
-        # Creating the plot
+        # Create the plot
         fig = None
         if plot_type in ['line', 'bar', 'scatter']:
+            segment_labels = [seg.split('_')[1] for seg in segments]  # Extracting segment labels
             if plot_type == 'line':
-                fig = px.line(x=df.columns[1:], y=segment_means.values, title=f"{selected_metric} over Segments")
+                fig = px.line(x=segment_labels, y=segment_means.values, title=f"{selected_metric} over Segments")
             elif plot_type == 'bar':
-                fig = px.bar(x=df.columns[1:], y=segment_means.values, title=f"{selected_metric} over Segments")
+                fig = px.bar(x=segment_labels, y=segment_means.values, title=f"{selected_metric} over Segments")
             elif plot_type == 'scatter':
-                fig = px.scatter(x=df.columns[1:], y=segment_means.values, title=f"{selected_metric} over Segments")
+                fig = px.scatter(x=segment_labels, y=segment_means.values, title=f"{selected_metric} over Segments")
 
             # Update layout for better readability
             fig.update_layout(xaxis_title="Segment", yaxis_title=selected_metric, xaxis={'type':'category'})
