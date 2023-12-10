44c6c04f83dbd3bad201d222aa64757bfe28e66f
edenede2 <131803027+edenede2@users.noreply.github.com>
2023-12-10 18:24:34 +0200

    Update rDoc_streamlit.py

--- rDoc_streamlit.py
+++ rDoc_streamlit.py
@@ -6,15 +6,10 @@ import openpyxl
 # Function to load and process the Excel file
 @st.cache_resource  # Updated caching function
 def load_excel(file):
-    df = pd.read_excel(file, header=0)
-    # Drop the row with NaN values which seems to be the second row in the file
-    df = df.drop(1)
-    # Reshape the DataFrame for easier processing
-    df = df.melt(id_vars=[df.columns[0]], var_name='Metric_Segment', value_name='Value')
-    df[['Metric', 'Segment']] = df['Metric_Segment'].str.split(' ', expand=True)
-    df = df.drop('Metric_Segment', axis=1)
-    # Convert values to numeric, errors coerced to NaN
-    df['Value'] = pd.to_numeric(df['Value'], errors='coerce')
+    # Read the file, skipping the first three rows and setting the first column as index
+    df = pd.read_excel(file, header=[0, 1], skiprows=[2], index_col=0)
+    # Create a multi-index using the metric and segment names
+    df.columns = pd.MultiIndex.from_tuples([tuple(c.split()) for c in df.columns])
     return df
 
 # Streamlit app
@@ -29,36 +24,38 @@ def main():
         st.write(df.head())
 
         # Metric selection
-        metrics = df['Metric'].unique()
+        metrics = df.columns.get_level_values(0).unique()
         selected_metric = st.selectbox("Select a Metric", metrics)
 
-        # Filter DataFrame for the selected metric
-        df_filtered = df[df['Metric'] == selected_metric]
+        # Segment names for the selected metric
+        segments = df[selected_metric].columns
 
         # Subject exclusion or isolation
-        subjects = df[df.columns[0]].unique()
+        subjects = df.index.unique()
         exclude_subjects = st.multiselect("Exclude Subjects", subjects)
         isolate_subject = st.selectbox("Or, Isolate a Single Subject (overrides exclusion)", ['None'] + list(subjects))
 
+        # Filtering DataFrame based on subject selection
         if isolate_subject != 'None':
-            df_filtered = df_filtered[df_filtered[df.columns[0]] == isolate_subject]
-        elif exclude_subjects:
-            df_filtered = df_filtered[~df_filtered[df.columns[0]].isin(exclude_subjects)]
+            df_filtered = df.loc[df.index == isolate_subject, selected_metric]
+        else:
+            df_filtered = df.loc[~df.index.isin(exclude_subjects), selected_metric]
 
         # Calculating mean values across subjects for each segment
-        segment_means = df_filtered.groupby('Segment')['Value'].mean().reset_index()
+        segment_means = df_filtered.mean()
 
         # Plot type selection
         plot_types = ['line', 'bar', 'scatter']
         plot_type = st.selectbox("Select Plot Type", plot_types)
 
         # Create the plot
+        fig = None
         if plot_type == 'line':
-            fig = px.line(segment_means, x='Segment', y='Value', title=f"{selected_metric} over Segments")
+            fig = px.line(x=segments, y=segment_means.values, title=f"{selected_metric} over Segments")
         elif plot_type == 'bar':
-            fig = px.bar(segment_means, x='Segment', y='Value', title=f"{selected_metric} over Segments")
+            fig = px.bar(x=segments, y=segment_means.values, title=f"{selected_metric} over Segments")
         elif plot_type == 'scatter':
-            fig = px.scatter(segment_means, x='Segment', y='Value', title=f"{selected_metric} over Segments")
+            fig = px.scatter(x=segments, y=segment_means.values, title=f"{selected_metric} over Segments")
 
         st.plotly_chart(fig)
 
