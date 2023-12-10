308c507ba44870271f9fcf1356ddbf7abe88dde0
edenede2 <131803027+edenede2@users.noreply.github.com>
2023-12-10 18:33:39 +0200

    Update rDoc_streamlit.py

--- rDoc_streamlit.py
+++ rDoc_streamlit.py
@@ -6,14 +6,20 @@ import openpyxl
 # Function to load and process the Excel file
 @st.cache_resource  # Updated caching function
 def load_excel(file):
-    # Read the file, skipping the first three rows and setting the first column as index
     df = pd.read_excel(file, header=[0, 1], skiprows=[2], index_col=0)
-    # The columns are already in a tuple format, so no need to split them
     return df
+
+# Function to detect outliers using IQR
+def detect_outliers(df, segment):
+    Q1 = df[segment].quantile(0.25)
+    Q3 = df[segment].quantile(0.75)
+    IQR = Q3 - Q1
+    outliers = df[(df[segment] < (Q1 - 1.5 * IQR)) | (df[segment] > (Q3 + 1.5 * IQR))]
+    return outliers.index
 
 # Streamlit app
 def main():
-    st.title("Excel Data Analysis App")
+    st.title("Excel Data Analysis App with Outlier Detection")
 
     # Upload Excel file
     uploaded_file = st.file_uploader("Upload your Excel file", type=["xlsx"])
@@ -34,11 +40,19 @@ def main():
         exclude_subjects = st.multiselect("Exclude Subjects", subjects)
         isolate_subject = st.selectbox("Or, Isolate a Single Subject (overrides exclusion)", ['None'] + list(subjects))
 
-        # Filtering DataFrame based on subject selection
+        # Exclude or isolate subjects
         if isolate_subject != 'None':
             df_filtered = df.loc[df.index == isolate_subject, selected_metric]
         else:
             df_filtered = df.loc[~df.index.isin(exclude_subjects), selected_metric]
+
+        # Detect and optionally exclude outliers
+        exclude_outliers = st.checkbox("Exclude Outliers")
+        outlier_indices = []
+        if exclude_outliers:
+            for segment in segments:
+                outlier_indices.extend(detect_outliers(df_filtered, segment))
+            df_filtered = df_filtered.drop(index=outlier_indices, errors='ignore')
 
         # Calculating mean values across subjects for each segment
         segment_means = df_filtered.mean()
@@ -55,6 +69,12 @@ def main():
             fig = px.bar(x=segments, y=segment_means.values, title=f"{selected_metric} over Segments")
         elif plot_type == 'scatter':
             fig = px.scatter(x=segments, y=segment_means.values, title=f"{selected_metric} over Segments")
+
+        # Highlight outliers if not excluded
+        if not exclude_outliers and outlier_indices:
+            for outlier in outlier_indices:
+                outlier_data = df.loc[outlier, selected_metric]
+                fig.add_trace(px.scatter(x=outlier_data.index, y=outlier_data.values, marker_color='red').data[0])
 
         st.plotly_chart(fig)
 
