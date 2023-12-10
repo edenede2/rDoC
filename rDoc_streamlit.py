c15703916606744e98e5409c8b360149a4edff4d
edenede2 <131803027+edenede2@users.noreply.github.com>
2023-12-10 18:45:25 +0200

    Update rDoc_streamlit.py

--- rDoc_streamlit.py
+++ rDoc_streamlit.py
@@ -16,18 +16,18 @@ def detect_outliers_iqr(df, segment):
     Q3 = df[segment].quantile(0.75)
     IQR = Q3 - Q1
     outliers = df[(df[segment] < (Q1 - 1.5 * IQR)) | (df[segment] > (Q3 + 1.5 * IQR))]
-    return outliers.index
+    return outliers
 
 # Function to detect outliers using STD
 def detect_outliers_std(df, segment):
     mean = df[segment].mean()
     std = df[segment].std()
     outliers = df[(df[segment] < (mean - 2 * std)) | (df[segment] > (mean + 2 * std))]
-    return outliers.index
+    return outliers
 
 # Streamlit app
 def main():
-    st.title("Excel Data Analysis App with Outlier Detection")
+    st.title("Excel Data Analysis App with Outlier Detection and Segment Removal")
 
     # Upload Excel file
     uploaded_file = st.file_uploader("Upload your Excel file", type=["xlsx"])
@@ -40,8 +40,9 @@ def main():
         metrics = df.columns.get_level_values(0).unique()
         selected_metric = st.selectbox("Select a Metric", metrics)
 
-        # Segment names for the selected metric
-        segments = df[selected_metric].columns
+        # Segment names for the selected metric and option to exclude specific segments
+        all_segments = df[selected_metric].columns
+        included_segments = st.multiselect("Include Segments", all_segments, default=all_segments)
 
         # Subject exclusion or isolation
         subjects = df.index.unique()
@@ -57,19 +58,25 @@ def main():
         # Outlier detection options
         outlier_method = st.selectbox("Select Outlier Detection Method", ["IQR", "STD"])
         exclude_outliers = st.checkbox("Exclude Outliers")
-        outlier_indices = set()
+        outlier_info = []
 
         if exclude_outliers:
-            for segment in segments:
+            for segment in included_segments:
                 if outlier_method == "IQR":
-                    outlier_indices.update(detect_outliers_iqr(df_filtered, segment))
+                    outliers = detect_outliers_iqr(df_filtered, segment)
                 elif outlier_method == "STD":
-                    outlier_indices.update(detect_outliers_std(df_filtered, segment))
+                    outliers = detect_outliers_std(df_filtered, segment)
+                
+                # Track outlier information
+                for index in outliers.index:
+                    outlier_info.append((index, segment))
 
+            # Remove outliers from the DataFrame
+            outlier_indices = [info[0] for info in outlier_info]
             df_filtered = df_filtered.drop(index=outlier_indices, errors='ignore')
 
         # Calculating mean values across subjects for each segment
-        segment_means = df_filtered.mean()
+        segment_means = df_filtered[included_segments].mean()
 
         # Plot type selection
         plot_types = ['line', 'bar', 'scatter']
@@ -78,19 +85,19 @@ def main():
         # Create the plot
         fig = None
         if plot_type == 'line':
-            fig = px.line(x=segments, y=segment_means.values, title=f"{selected_metric} over Segments")
+            fig = px.line(x=included_segments, y=segment_means.values, title=f"{selected_metric} over Segments")
         elif plot_type == 'bar':
-            fig = px.bar(x=segments, y=segment_means.values, title=f"{selected_metric} over Segments")
+            fig = px.bar(x=included_segments, y=segment_means.values, title=f"{selected_metric} over Segments")
         elif plot_type == 'scatter':
-            fig = px.scatter(x=segments, y=segment_means.values, title=f"{selected_metric} over Segments")
-
-        # Highlight outliers if not excluded
-        if not exclude_outliers and outlier_indices:
-            for outlier in outlier_indices:
-                outlier_data = df.loc[outlier, selected_metric]
-                fig.add_trace(px.scatter(x=outlier_data.index, y=outlier_data.values, marker_color='red').data[0])
+            fig = px.scatter(x=included_segments, y=segment_means.values, title=f"{selected_metric} over Segments")
 
         st.plotly_chart(fig)
+
+        # Displaying removed outliers
+        if outlier_info:
+            st.write("Removed Outliers:")
+            for subject, segment in outlier_info:
+                st.write(f"Subject: {subject}, Segment: {segment}")
 
 if __name__ == "__main__":
     main()
