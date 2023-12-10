7d6a3bf5349ae9aa15e647f511bfad5257760c0d
edenede2 <131803027+edenede2@users.noreply.github.com>
2023-12-10 18:20:24 +0200

    Update rDoc_streamlit.py

--- rDoc_streamlit.py
+++ rDoc_streamlit.py
@@ -6,13 +6,15 @@ import openpyxl
 # Function to load and process the Excel file
 @st.cache_resource  # Updated caching function
 def load_excel(file):
-    # Reading the Excel file and using the first row for multi-index columns
-    df = pd.read_excel(file, header=[0, 1])
-    # Flatten the MultiIndex for easier processing
-    df.columns = ['_'.join(col).strip() for col in df.columns.values]
-    # Convert all relevant columns to numeric, ignoring errors to skip non-numeric columns
-    for col in df.columns[1:]:  # Assuming first column is not numeric
-        df[col] = pd.to_numeric(df[col], errors='coerce')
+    df = pd.read_excel(file, header=0)
+    # Drop the row with NaN values which seems to be the second row in the file
+    df = df.drop(1)
+    # Reshape the DataFrame for easier processing
+    df = df.melt(id_vars=[df.columns[0]], var_name='Metric_Segment', value_name='Value')
+    df[['Metric', 'Segment']] = df['Metric_Segment'].str.split(' ', expand=True)
+    df = df.drop('Metric_Segment', axis=1)
+    # Convert values to numeric, errors coerced to NaN
+    df['Value'] = pd.to_numeric(df['Value'], errors='coerce')
     return df
 
 # Streamlit app
@@ -24,48 +26,41 @@ def main():
     if uploaded_file is not None:
         df = load_excel(uploaded_file)
         st.write("DataFrame Preview:")
-        st.write(df)
+        st.write(df.head())
 
-        # Extract metric names (assuming they are prefixes in the column names)
-        metric_names = list(set(col.split('_')[0] for col in df.columns if '_' in col))
-        selected_metric = st.selectbox("Select a Metric", metric_names)
+        # Metric selection
+        metrics = df['Metric'].unique()
+        selected_metric = st.selectbox("Select a Metric", metrics)
 
-        # Extract segment names for the selected metric
-        segments = [col for col in df.columns if col.startswith(selected_metric)]
+        # Filter DataFrame for the selected metric
+        df_filtered = df[df['Metric'] == selected_metric]
+
+        # Subject exclusion or isolation
+        subjects = df[df.columns[0]].unique()
+        exclude_subjects = st.multiselect("Exclude Subjects", subjects)
+        isolate_subject = st.selectbox("Or, Isolate a Single Subject (overrides exclusion)", ['None'] + list(subjects))
+
+        if isolate_subject != 'None':
+            df_filtered = df_filtered[df_filtered[df.columns[0]] == isolate_subject]
+        elif exclude_subjects:
+            df_filtered = df_filtered[~df_filtered[df.columns[0]].isin(exclude_subjects)]
+
+        # Calculating mean values across subjects for each segment
+        segment_means = df_filtered.groupby('Segment')['Value'].mean().reset_index()
 
         # Plot type selection
         plot_types = ['line', 'bar', 'scatter']
         plot_type = st.selectbox("Select Plot Type", plot_types)
 
-        # Subject exclusion or isolation
-        subjects = df['Subject'].unique()
-        exclude_subjects = st.multiselect("Exclude Subjects", subjects)
-        isolate_subject = st.selectbox("Or, Isolate a Single Subject (overrides exclusion)", ['None'] + list(subjects))
-
-        # Data processing for visualization
-        if isolate_subject != 'None':
-            df_filtered = df[df['Subject'] == isolate_subject]
-        else:
-            df_filtered = df if not exclude_subjects else df[~df['Subject'].isin(exclude_subjects)]
-
-        # Calculating mean across all subjects for each segment under the selected metric
-        segment_means = df_filtered[segments].mean()
-
         # Create the plot
-        fig = None
-        if plot_type in ['line', 'bar', 'scatter']:
-            segment_labels = [seg.split('_')[1] for seg in segments]  # Extracting segment labels
-            if plot_type == 'line':
-                fig = px.line(x=segment_labels, y=segment_means.values, title=f"{selected_metric} over Segments")
-            elif plot_type == 'bar':
-                fig = px.bar(x=segment_labels, y=segment_means.values, title=f"{selected_metric} over Segments")
-            elif plot_type == 'scatter':
-                fig = px.scatter(x=segment_labels, y=segment_means.values, title=f"{selected_metric} over Segments")
+        if plot_type == 'line':
+            fig = px.line(segment_means, x='Segment', y='Value', title=f"{selected_metric} over Segments")
+        elif plot_type == 'bar':
+            fig = px.bar(segment_means, x='Segment', y='Value', title=f"{selected_metric} over Segments")
+        elif plot_type == 'scatter':
+            fig = px.scatter(segment_means, x='Segment', y='Value', title=f"{selected_metric} over Segments")
 
-            # Update layout for better readability
-            fig.update_layout(xaxis_title="Segment", yaxis_title=selected_metric, xaxis={'type':'category'})
-
-            st.plotly_chart(fig)
+        st.plotly_chart(fig)
 
 if __name__ == "__main__":
     main()
